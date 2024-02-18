46f8020aecd963cc918b5c79e59ccf490580b1f6
Luca <lucamug@gmail.com>
2024-02-18 21:16:23 +0900

    no message

--- src/Year2023/Others.elm
+++ src/Year2023/Others.elm
@@ -24,6 +24,15 @@ type Type_
     | Presentation
 
 
+
+--
+-- Add this too?
+--
+-- href="https://www.elmweekly.nl/p/a-game-jam" data-testid="post-preview-title">A Game Jam!
+-- href="https://www.elmweekly.nl/p/a-game-jam">I forgot to get this in this weeks newsletter and next week will be too late since it starts on Friday the 8th! Elm Game Jam #6 A week long game jam…
+-- datetime="2023-09-07T17:18:42.223Z">Sep 8, 2023
+
+
 data : List Data
 data =
     []
--- src/Year2023/Youtube.elm
+++ src/Year2023/Youtube.elm
@@ -14,7 +14,36 @@ type alias Data =
 
 data : List Data
 data =
-    []
+    --
+    --
+    -- From https://www.youtube.com/results?search_query=elmlang&sp=EgIIBQ%253D%253D
+    --
+    --
+    [ { descriptions = "A quick look at Elm: Advantages and disadvantages of JavaScript; Basic Elm project example; Why use Elm?"
+      , id = "wPiq8ka42uY"
+      , title = "Could Elm replace JavaScript?"
+      , by = "Awesome"
+      , event = ""
+      , datePublished = "2023-05-30"
+      , dateEvent = ""
+      }
+    , { descriptions = "In this video, we'll cover our experience using Elm and it's hidden superpowers. Elm is a pure-functional programming language designed for building web applications."
+      , id = "PNgAmr4gPYQ"
+      , title = "Is Elm Dead? 💀 Discover the programming language's hidden potential."
+      , by = "SwiftCom"
+      , event = ""
+      , datePublished = "2023-08-14"
+      , dateEvent = ""
+      }
+    , { descriptions = "The video is a presentation on Elm, a functional programming language for frontend development that compiles to JavaScript."
+      , id = "SAAJlqu2sos"
+      , title = "Elm - FUNctional language for frontend"
+      , by = "Tomas Latal"
+      , event = ""
+      , datePublished = "2023-05-26"
+      , dateEvent = ""
+      }
+    ]
 
 
 dataOld : List Data
